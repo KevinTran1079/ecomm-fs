bbd1cd1d57c65b5d98148f5bfa211df1dd86d647
Kevin Tran <187645416+KevinTran1079@users.noreply.github.com>
2025-05-14 10:46:27 -0500

    bug fix setting the default sidebar view to closed instead of open

--- ecomm/src/app/(home)/navbar.tsx
+++ ecomm/src/app/(home)/navbar.tsx
@@ -51,7 +51,7 @@ const navbarItems = [
 
 export const Navbar = () => {
     const pathname = usePathname();
-    const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+    const [isSideBarOpen, setIsSideBarOpen] = useState(false);
     return (
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
             <Link href = "/" className = "pl-6 flex items-center">
